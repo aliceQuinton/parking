ac39fac0fc1bb13af208cf7db52e03cce220e74d
Alice Quinton <quinton.alice@gmail.com>
2022-08-18 01:13:53 +0200

    méthode pour avoir la liste des parkings
    méthode pour avoir la liste des parkings avec retour null provisoire

--- parking/src/main/java/com/formation/parking/controllers/ParkingController.java
+++ parking/src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -1,5 +1,13 @@
 package com.formation.parking.controllers;
 
+import java.util.List;
+
+import com.formation.parking.models.Parking;
+
 public class ParkingController {
+	
+	public List<Parking> getListeParkings(){
+		return null;
+	}
 
 }
