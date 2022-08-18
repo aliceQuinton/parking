85b8cda023f8015a84ab2c9c228f2bd0ca05f607
Alice Quinton <quinton.alice@gmail.com>
2022-08-18 16:22:03 +0200

    interface ParkingAPIDAO + ResponseParkingAPIEntity + RecordEntity +
    interface ParkingAPIDAO avec méthode + ResponseParkingAPIEntity qui récupère les enregistrements de l'API + RecordEntity objet enregistrement + FieldsEntity objet champ. Vides avant initialisation.

---
+++ parking/src/main/java/com/formation/parking/dao/entity/FieldsEntity.java
@@ -0,0 +1,67 @@
+package com.formation.parking.dao.entity;
+
+public class FieldsEntity {
+
+	private String idObj;
+	
+	private String grpNom;
+	
+	private String grpExploitation;
+	
+	private String grpDisponible;
+	
+	private String grpStatut;
+	
+	private String grpHorodatage;
+
+	public String getIdObj() {
+		return idObj;
+	}
+
+	public void setIdObj(String idObj) {
+		this.idObj = idObj;
+	}
+
+	public String getGrpNom() {
+		return grpNom;
+	}
+
+	public void setGrpNom(String grpNom) {
+		this.grpNom = grpNom;
+	}
+
+	public String getGrpExploitation() {
+		return grpExploitation;
+	}
+
+	public void setGrpExploitation(String grpExploitation) {
+		this.grpExploitation = grpExploitation;
+	}
+
+	public String getGrpDisponible() {
+		return grpDisponible;
+	}
+
+	public void setGrpDisponible(String grpDisponible) {
+		this.grpDisponible = grpDisponible;
+	}
+
+	public String getGrpStatut() {
+		return grpStatut;
+	}
+
+	public void setGrpStatut(String grpStatut) {
+		this.grpStatut = grpStatut;
+	}
+
+	public String getGrpHorodatage() {
+		return grpHorodatage;
+	}
+
+	public void setGrpHorodatage(String grpHorodatage) {
+		this.grpHorodatage = grpHorodatage;
+	}
+	
+	
+	
+}
---
+++ parking/src/main/java/com/formation/parking/dao/entity/ReponseParkingAPIEntity.java
@@ -0,0 +1,19 @@
+package com.formation.parking.dao.entity;
+
+import java.util.List;
+
+public class ReponseParkingAPIEntity {
+	
+	private List<RecordEntity> records;
+
+	public List<RecordEntity> getRecords() {
+		return records;
+	}
+
+	public void setRecords(List<RecordEntity> records) {
+		this.records = records;
+	}
+	
+	
+
+}
