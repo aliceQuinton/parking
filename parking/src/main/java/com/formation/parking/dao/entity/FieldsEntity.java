816a823ffa8c4da4331c7222b43ecddd7e625c15
Alice Quinton <quinton.alice@gmail.com>
2022-08-18 17:04:54 +0200

    mise en place de l'ignorance des autres variables de l'API
    mise en place de l'ignorance des variables dont on ne souhaite pas tenir compte de l'API via l'annotation @JsonIgnoreProperties avec l'option (ignoreUnknown = true)

--- parking/src/main/java/com/formation/parking/dao/entity/FieldsEntity.java
+++ parking/src/main/java/com/formation/parking/dao/entity/FieldsEntity.java
@@ -1,7 +1,10 @@
 package com.formation.parking.dao.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FieldsEntity {
 
 	@JsonProperty( value ="idobj")
--- parking/src/main/java/com/formation/parking/dao/entity/ReponseParkingAPIEntity.java
+++ parking/src/main/java/com/formation/parking/dao/entity/ReponseParkingAPIEntity.java
@@ -2,8 +2,10 @@ package com.formation.parking.dao.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReponseParkingAPIEntity {
 	
 	@JsonProperty( value ="records")
